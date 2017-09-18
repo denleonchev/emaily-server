e9990854c42e8352fd609acca258e1b3ef60096d
Denys Leonchev <dleo@softserveinc.com>
2017-09-18 18:18:35 +0300

    Continue

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 import Header from './Header'
 
@@ -13,9 +16,13 @@ const Landing = () => (
     <h2>Landing</h2>
 )
 
-const App = () => {
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
     return (
-        <div>
+        <div className="container">
             <Router>
                 <div>
                     <Header />
@@ -26,6 +33,15 @@ const App = () => {
             </Router>
         </div>
     );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUser: () => {
+      dispatch(actions.fetchUser());
+    }
+  }
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
+import 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(...[thunk]));
 
 ReactDom.render(
     <Provider store={store}>
