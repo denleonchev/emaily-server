f0c3704f6081e1b090377295433e5ed13623062f
denleonchev <den.leonchev@gmail.com>
2017-09-21 00:55:45 +0300

    Completed billing system

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,6 +20,9 @@ class Header extends Component {
           return [
             <li key="1"><Payments /></li>,
             <li key="2">
+              Credits: {this.props.auth.credits}
+            </li>,
+            <li key="3">
               <a href="/api/logout">Logout</a>
             </li>
           ];
