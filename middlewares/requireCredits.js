d7709b7955316931c82c92f7fca6f1cbdd5a7c0a
Denys Leonchev <dleo@softserveinc.com>
2017-10-04 22:07:52 +0300

    Enhanced mail

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
     if(!req.user.credits) {
-        console.log('req.user.redits', req.user.redits)
         return res.status(403).send({error: 'Not enough cretis'});
     }
 
