42246d3102a5069f2f0f3308345bf2b49a6aad8e
Denys Leonchev <dleo@softserveinc.com>
2017-10-12 15:42:37 +0300

    still working

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const { URL } = require('url');
+const Path = require('path-parser');
+const _ = require('lodash');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -7,9 +11,6 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-    app.get('/api/surveys/ty', (req, res) => {
-      res.send('Thanks for voting!');
-    })
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients} = req.body;
@@ -35,6 +36,21 @@ module.exports = (app) => {
       });
 
       app.post('/api/surveys/webhooks', (req, res) => {
-        console.log(req.body);
+        const events = req.body.map(({email, url}) => {
+          const pathname = new URL(url).pathname;
+          const p = new Path('/api/surveys/:surveyId/:choice');
+          const match = p.test(pathname);
+          if(match) {
+            return {
+              email,
+              surveyId: match.surveyId,
+              choice: match.choice
+            }
+          }
+        });
+        const compactEvents = events.filter((event) => (event));
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+        console.log('compactEvents', compactEvents);
+        console.log('uniqueEvents', uniqueEvents);
       })
 };
--- sendgrid_webhook_windows.js
+++ sendgrid_webhook_windows.js
@@ -1,6 +1,6 @@
 var localtunnel = require('localtunnel');
 localtunnel(5000, { subdomain: 'sundrenchedone45' }, function(err, tunnel) {
+  console.log('==================================')
   console.log('LT running')
-  console.log('error', err)
-  console.log('tunnel', tunnel)
+  console.log('==================================')
 });
